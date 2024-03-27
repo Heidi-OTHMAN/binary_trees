2d0deaa71ecf72859709dea3c565fdbae69e65d1
Heidi-OTHMAN <heidiothman355@gmail.com>
2024-03-26 19:55:26 -0700

    a

--- binary_trees.h
+++ binary_trees.h
@@ -7,6 +7,29 @@
 #include <string.h>
 #include <limits.h>
 
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -83,5 +106,9 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int heap_extract(heap_t **root);
+heap_t *array_to_heap(int *array, size_t size);
+heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* _BINARY_TREES_H_ */
